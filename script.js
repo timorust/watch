eb3bbb04096d566fa33183466b7da12d49b054c8
timor.rust <timorust1@gmail.com>
2021-02-01 09:17:01 +0200

    final
    finish

--- script.js
+++ script.js
@@ -1,36 +1,85 @@
 // window.onload = () => {}
+// elements
+const $container = document.getElementById('container');
+const $timer = document.getElementById('timer');
+const $counterNumber = document.getElementById('counter-number');
+const $clicker = document.getElementById('clicker');
+const $message = document.getElementById('message');
+const $timerScore = document.getElementById('timer-score');
+const $reloadButt = document.getElementById('reload');
+const $startButt = document.getElementById('start');
 
-const timeWatch = document.getElementById('timer');
-// console.log(TBD);
-const counterNumber = document.getElementById('counter-number');
-const getClicker = document.getElementById('clicker');
-const getMessage = document.getElementById('message');
+// game rules
+const winScore = 10;
+const maxTime = 5;
 
+// timers
+let clicksCnt;
+let timerCnt;
+let timerInterval;
 
-let clicksCnt = 0;
+// function
+const init = ($event) => {
+	const fromBuutId = $event.target.id;
+	clicksCnt = 0;
+	timerCnt = maxTime;
+	$timer.innerHTML = timerCnt;
+	$counterNumber.innerHTML = clicksCnt;
+	$message.classList.remove('message--failed');
+	$message.classList.remove('message--success');
 
-getClicker.addEventListener('click', () => {
-    clicksCnt++;
-    counterNumber.innerHTML = clicksCnt;
-});
-
-let timerCnt = 5;
-
-const myTimerInterval = setInterval(() => {
-	timerCnt--;
-	timeWatch.innerHTML = timerCnt;
-
-	if(timerCnt === 0) {
-
-		clearInterval(myTimerInterval);
-
-		if(clicksCnt >= 20) {
-
-getMessage.classList.add('message--success');
-
-} else{
-	getMessage.classList.add('message--failed');
-}		
-
+	if(fromBuutId === 'start') {
+		timerInterval = setInterval(cntDounTimeAndLose, 1000);
+		$container.classList.add('started');
 	}
-}, 1000);
+	
+}
+
+
+
+
+
+const cntDounTimeAndLose = () => {
+	timerCnt--;
+	$timer.innerHTML = timerCnt;
+	checkGameStatus();
+}
+
+const cntUpClicksAndWin = () => {
+	clicksCnt++;  
+	$counterNumber.innerHTML = clicksCnt; 
+   checkGameStatus();
+}
+
+const stopGame = () => {
+	clearInterval(timerInterval);
+	$container.classList.remove('started');
+}
+
+const checkGameStatus = () => {
+	//lose
+	if(timerCnt === 0) {
+		stopGame();
+		if(clicksCnt < winScore) {
+			 $message.classList.add('message--failed');
+			}		
+	}
+	//clicks
+	if(clicksCnt >= winScore){
+		stopGame();
+		$message.classList.add('message--success'); 
+		$timerScore.innerHTML = 'You time:' + (maxTime - timerCnt) + ' seconds';
+	}
+
+}
+
+
+
+
+// listener
+$clicker.addEventListener('click',cntUpClicksAndWin);
+$reloadButt.addEventListener('click', init);
+$startButt.addEventListener('click', init);
+
+
+// init();
